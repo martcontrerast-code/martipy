d181e486a11070b5b39844746c7de83d1e4640ff
martcontrerast-code <mart.contrerast@duocuc.cl>
2026-04-17 18:10:40 -0400

    ejercicios for

---
+++ martin.py
@@ -0,0 +1,49 @@
+# suma=0
+# notas=int(input("ingrese la cantidad de notas "))
+# for i in range(notas):
+#     nota=float(input("ingrese su nota "))
+#     suma=suma+nota
+#     prom=suma/notas
+#     print(f"El promedio es {prom} ")
+
+#     if prom>=4:
+#         print("el alumno ha aprovado")
+#     else :
+#         print("el alumno reprobo")
+
+# num=int(input("ingrese numero:"))
+# total=0
+# for i in range(num):
+#     total=total+i+1
+#     print(f"la sumatoria es: {total}")
+
+# num=int(input("ingrese numero:"))
+# total=1
+# for i in range(num):
+#     total=total*(i+1)
+#     print(f"la sumatoria es: {total}")
+
+
+
+# for i in "lol":
+#     print(i)
+
+
+#contador de vovales y consonantes
+# vocales=0
+# conso=0
+# nombre=(input("ingrese su nombre: "))
+# for i in nombre:
+#     print (i)
+#     if i in "aeiouAEIOU":
+#         vocales+=1
+#     elif i ==" ":
+#         ''''''
+#     else:
+#         conso+=1
+# print(f"el total de vocales es {vocales}")
+# print(f"el total de consonantes es {conso}")
+
+
+
+
